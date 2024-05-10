48ff50040828b0f9a01467204663854a011d83db
天上繁星 <1198782192@qq.com>
2024-05-10 18:20:55 +0800

    chore: 更新编译配置

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -5,6 +5,8 @@ import { zh } from './zh'
 
 export default defineConfig({
   ...shared,
+  outDir:'../public',
+  base:'/mapbox-gl-draw-plus/',
   locales: {
     root: { label: 'English', ...en },
     zh: { label: '简体中文', ...zh }
